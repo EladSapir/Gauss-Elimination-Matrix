8351a896b52ba38ad5b4098919971b94c2a94795
Elad Sapir <elad.1999.1999@gmail.com>
2022-03-28 22:47:47 +0300

    Matrix multiply-will be used with gauss elimination

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def matrixMul(matrixA, matrixB):
+    """
+    A Function that gets 2 matrices and checks if they are: 1. Square matrix
+                                                            2. In the same size
+    ** If one of the two above is False the program will stop **
+    Then the function will multiply them according to the according to the multiplication algorithm
+    :param matrixA: The matrix at the left of the multiplication
+    :param matrixB:The matrix at the right of the multiplication
+    :return: A new matrix, it represents the results
+    """
+    if len(matrixA) == len(matrixB) and len(matrixA) == len(matrixA[0]):  # checks the sizes of the both matrices
+        for i in range(len(matrixA)):  # checks if the left matrix is square
+            if len(matrixA) != len(matrixA[i]):
+                raise "Not NxN"
+        for i in range(len(matrixB)):  # checks if the left matrix is square
+            if len(matrixB) != len(matrixB[i]):
+                raise "Not NxN"
+        matrixC =[[0 for x in range(len(matrixA))] for y in range(len(matrixB))]  # Generating a new matrix for the result
+        for i in range(len(matrixC)):  # multiplication according to the multiplication algorithm
+            for j in range(len(matrixC)):
+                for k in range(len(matrixC)):
+                    matrixC[i][j] += matrixA[i][k] * matrixB[k][j]
+        return matrixC
+    else:
+        raise "Not NxN / same size"
